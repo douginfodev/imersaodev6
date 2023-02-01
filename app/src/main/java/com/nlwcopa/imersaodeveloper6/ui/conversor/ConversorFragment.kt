e97049194b3a1d7b6085c62bf6f864c385a3e26a
= <=>
2023-02-01 00:37:39 -0300

    Add Styles buttib default and colors

--- app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/ConversorFragment.kt
+++ app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/ConversorFragment.kt
@@ -1,11 +1,23 @@
 package com.nlwcopa.imersaodeveloper6.ui.conversor
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.nlwcopa.imersaodeveloper6.R
 import com.nlwcopa.imersaodeveloper6.ui.main.MainFragment
 
 class ConversorFragment : Fragment () {
 
     companion object {
         fun newInstance() = ConversorFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_conversor, container, false)
     }
 }
