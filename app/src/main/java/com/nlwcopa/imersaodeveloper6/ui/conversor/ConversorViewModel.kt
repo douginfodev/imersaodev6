c56701b0e7b057cd519bd3bf7fbc987c8f42257b
= <=>
2023-02-01 16:43:53 -0300

    Add dataBinding Gradle

--- app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/ConversorViewModel.kt
+++ app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/ConversorViewModel.kt
@@ -1,6 +1,22 @@
 package com.nlwcopa.imersaodeveloper6.ui.conversor
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ConversorViewModel : ViewModel() {
+
+    private val _resultado = MutableLiveData<String>().apply {
+        value = "Crie seu próprio bolão da Copa e compartilhe entre amigos!"
+    }
+    val resultado: LiveData<String> = _resultado
+
+    //FUNCTIONS
+    fun onConvert() {
+        Log.i("INFO","Valor Convertido")
+        //Toast.makeText(this, "Game has just finished", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
