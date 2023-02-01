85f09d741d09b5628c419194663e47afa1881842
= <=>
2023-01-31 22:18:55 -0300

    Created Styles file and colors , strings updated

--- app/src/main/java/com/nlwcopa/imersaodeveloper6/MainActivity.kt
+++ app/src/main/java/com/nlwcopa/imersaodeveloper6/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nlwcopa.imersaodeveloper6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nlwcopa.imersaodeveloper6.ui.conversor.ConversorFragment
 import com.nlwcopa.imersaodeveloper6.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.container, ConversorFragment.newInstance())
                 .commitNow()
         }
     }
--- app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/ConversorFragment.kt
+++ app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/ConversorFragment.kt
@@ -1,6 +1,11 @@
 package com.nlwcopa.imersaodeveloper6.ui.conversor
 
 import androidx.fragment.app.Fragment
+import com.nlwcopa.imersaodeveloper6.ui.main.MainFragment
 
 class ConversorFragment : Fragment () {
+
+    companion object {
+        fun newInstance() = ConversorFragment()
+    }
 }
