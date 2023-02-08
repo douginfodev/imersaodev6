79f67043411fcd431455ae786a3c7eaa7f4208b7
= <=>
2023-02-08 19:01:15 -0300

    Created RadioButton Moeda Options

--- app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/moedas/MoedasFragment.kt
+++ app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/moedas/MoedasFragment.kt
@@ -8,23 +8,54 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.nlwcopa.imersaodeveloper6.R
 import com.nlwcopa.imersaodeveloper6.databinding.FragmentConversorBinding
+import com.nlwcopa.imersaodeveloper6.databinding.FragmentMoedasBinding
 import com.nlwcopa.imersaodeveloper6.ui.conversor.ConversorFragment
 import com.nlwcopa.imersaodeveloper6.ui.conversor.ConversorViewModel
 import com.nlwcopa.imersaodeveloper6.ui.main.MainViewModel
 
 class MoedasFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MoedasFragment()
-    }
-
-    private lateinit var viewModel: MoedasViewModel
-
+  /*  private lateinit var viewModel: MoedasViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_moedas, container, false)
+    }*/
+
+    private var _binding: FragmentMoedasBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = MoedasFragment()
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        //ViewModel
+        val moedasViewModel =
+            ViewModelProvider(this).get(MoedasViewModel::class.java)
+
+        //Inserir Fragment na View
+        _binding = FragmentMoedasBinding.inflate(inflater, container, false)
+        val root : View = binding.root
+
+        //binding.nome da variable no xml =
+        // nome da val do viewModelProvider
+        binding.moedasViewModel  = moedasViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 }
--- app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/moedas/MoedasViewModel.kt
+++ app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/moedas/MoedasViewModel.kt
@@ -1,8 +1,35 @@
 package com.nlwcopa.imersaodeveloper6.ui.conversor.moedas
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MoedasViewModel : ViewModel() {
 
 //  android:paddingTop="?attr/actionBarSize"
+//private val moedas1 = arrayOf(true,false,false)
+//private val moedas2 = arrayListOf(false,true,false)
+private var _moedas1 = MutableLiveData<ArrayList<Boolean>>()
+    val moedas1: LiveData<ArrayList<Boolean>>
+        get() = _moedas1
+
+  //Moedas 1
+    private val _rdbbtn1 = MutableLiveData<Boolean>()
+    val rdbbtn1: LiveData<Boolean>
+        get() = _rdbbtn1
+
+    private val _rdbbtn2 = MutableLiveData<Boolean>()
+    val rdbbtn2: LiveData<Boolean>
+        get() = _rdbbtn2
+
+    private val _rdbbtn3 = MutableLiveData<Boolean>()
+    val rdbbtn3: LiveData<Boolean>
+        get() = _rdbbtn3
+
+    init{
+        _rdbbtn1.value = true
+        _rdbbtn2.value = false
+        _rdbbtn3.value = false
+    }
+
 }
