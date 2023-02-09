baf300685791d9e71bf932306ec5b852acf2a51f
= <=>
2023-02-09 10:57:27 -0300

    Updated Gradle Navigations dependencies

--- app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/moedas/MoedasViewModel.kt
+++ app/src/main/java/com/nlwcopa/imersaodeveloper6/ui/conversor/moedas/MoedasViewModel.kt
@@ -6,14 +6,14 @@ import androidx.lifecycle.ViewModel
 
 class MoedasViewModel : ViewModel() {
 
-//  android:paddingTop="?attr/actionBarSize"
+    //  android:paddingTop="?attr/actionBarSize"
 //private val moedas1 = arrayOf(true,false,false)
 //private val moedas2 = arrayListOf(false,true,false)
-private var _moedas1 = MutableLiveData<ArrayList<Boolean>>()
+    private var _moedas1 = MutableLiveData<ArrayList<Boolean>>()
     val moedas1: LiveData<ArrayList<Boolean>>
         get() = _moedas1
 
-  //Moedas 1
+    //Moedas 1
     private val _rdbbtn1 = MutableLiveData<Boolean>()
     val rdbbtn1: LiveData<Boolean>
         get() = _rdbbtn1
@@ -26,10 +26,52 @@ private var _moedas1 = MutableLiveData<ArrayList<Boolean>>()
     val rdbbtn3: LiveData<Boolean>
         get() = _rdbbtn3
 
-    init{
+    //Moedas 2
+    private val _rdbbtn4 = MutableLiveData<Boolean>()
+    val rdbbtn4: LiveData<Boolean>
+        get() = _rdbbtn4
+
+    private val _rdbbtn5 = MutableLiveData<Boolean>()
+    val rdbbtn5: LiveData<Boolean>
+        get() = _rdbbtn5
+
+    private val _rdbbtn6 = MutableLiveData<Boolean>()
+    val rdbbtn6: LiveData<Boolean>
+        get() = _rdbbtn6
+
+    init {
         _rdbbtn1.value = true
         _rdbbtn2.value = false
         _rdbbtn3.value = false
+
+        _rdbbtn4.value = false
+        _rdbbtn5.value = true
+        _rdbbtn6.value = false
     }
 
+    fun onChangeColor(op: Int) {
+        _rdbbtn1.value = false
+        _rdbbtn2.value = false
+        _rdbbtn3.value = false
+
+        when (op) {
+            11 -> _rdbbtn1.value = true
+            12 -> _rdbbtn2.value = true
+            13 -> _rdbbtn3.value = true
+            else -> _rdbbtn1.value = true
+        }
+    }
+
+    fun onChangeColorConvert(op: Int) {
+        _rdbbtn4.value = false
+        _rdbbtn5.value = false
+        _rdbbtn6.value = false
+
+        when (op) {
+            14 -> _rdbbtn4.value = true
+            15 -> _rdbbtn5.value = true
+            16 -> _rdbbtn6.value = true
+            else -> _rdbbtn4.value = true
+        }
+    }
 }
